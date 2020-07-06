626487fbc42ac8753eae9aaa48af53e750b6c523
Younjin Jeong <younjin.jeong@gmail.com>
2020-07-06 23:31:15 +0800

    initial commit

---
+++ src/main/java/io/jaylee/spring/custominitializr/CustomInitializrApplication.java
@@ -0,0 +1,13 @@
+package io.jaylee.spring.custominitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomInitializrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jaylee/spring/custominitializr/CustomInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jaylee.spring.custominitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomInitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
